2a27f886aa97f80de9a4b7bff8564f1dbac8c4c4
Exempti0n <sj990927@gmail.com>
2022-04-28 16:28:28 +0900

    1

---
+++ QuotesGenrator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenrator
+//
+//  Created by 오승준 on 2022/03/31.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
--- QuotesGenrator/ViewController.swift
+++ QuotesGenrator/ViewController.swift
@@ -3,17 +3,31 @@
 //  QuotesGenrator
 //
 //  Created by 오승준 on 2022/03/31.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    let quotes = [
+        Quote(contents: "명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1", name: "이름1"),
+        Quote(contents: "명언2명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1", name: "이름2"),
+        Quote(contents: "명언3명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1", name: "이름3"),
+        Quote(contents: "명언4명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1", name: "이름4"),
+        Quote(contents: "명언5명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1명언1", name: "이름5"),
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func tapQuoteGeneratorButton(_ sender: Any) {
+        let random = Int(arc4random_uniform(5)) //0~4 난수 생성
+        let quote = quotes[random]
+        self.quoteLabel.text = quote.contents
+        self.nameLabel.text = quote.name
+    }
 }
 
